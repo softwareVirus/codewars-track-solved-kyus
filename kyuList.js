a13c541a7da25a07f5c7c0c8d5b45c0860397264
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2023-07-02 02:54:31 +0300

    Initial commit

---
+++ kyuList.js
@@ -0,0 +1,43 @@
+let user = window.location.href.split('?')[1].split('=')[1]
+let kyus = [0,0,0,0,0,0,0,0]
+let loading = true
+const button = document.getElementsByTagName('button').item(0)
+button.addEventListener('click' ,() => {
+    window.location.href = 'index.html'
+})
+async function main() {
+    
+    let page = 0
+    let data = []
+    let user_data = await fetch(`https://www.codewars.com/api/v1/users/${user}/code-challenges/completed?page=${page}`)
+    user_data = await user_data.json()
+    data.push(...user_data.data)
+    page++
+    while(user_data.data.length === 200) {
+        user_data = await fetch(`https://www.codewars.com/api/v1/users/${user}/code-challenges/completed?page=${page}`)
+        user_data = await user_data.json()
+        page++
+        data.push(...user_data.data)
+    }
+    console.log(data.length)
+    let index = 0
+    while(1) {
+        let kyu_data = await fetch(`https://www.codewars.com/api/v1/code-challenges/${data[index].id}`)
+        kyu_data = await kyu_data.json()
+        kyus[Math.abs(kyu_data.rank.id)-1]++
+        index++
+        if(index === data.length) {
+            let number_of_solved_kyus = document.getElementsByClassName('number-of-solved')
+            kyus.forEach((item,index) => {
+                number_of_solved_kyus.item(7-index).textContent = item + ' kyus solved'
+            })
+            break
+        }
+    }
+    let title = document.getElementById('user-title')
+    title.textContent = `${user} History`
+    document.getElementById('display-number-of-kyus').className = ''
+    document.getElementById('loading-section').className = 'hide-element'
+}
+
+main()
---
+++ queryKyus.js
@@ -0,0 +1,44 @@
+const app_title = document.getElementById("title");
+const title = "Query Number of Solved Kyus on Codewars";
+
+(async () => {
+  //animation to write the letters of title one by one
+  for (let item of title) {
+    const old_title = app_title.children.item(0).textContent;
+    app_title.children.item(0).textContent =
+      old_title.slice(0, old_title.length - 1) + item + "|";
+    console.log(app_title.children.item(0).textContent);
+    await new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        clearInterval(interval);
+        resolve();
+      }, 40);
+    });
+  }
+  //remove selector from title
+  const tmp_title = app_title.children.item(0).textContent;
+  console.log(tmp_title.slice(0, -1));
+  app_title.children.item(0).textContent = tmp_title.slice(0, -1);
+  //event listener to click enter when typing in search bar
+  const search_bar = document.getElementById('search')
+  search_bar.addEventListener('keydown',(event) => {
+    if(event.key === 'Enter') {
+        fetch(`https://www.codewars.com/api/v1/users/${event.target.value}`)
+        .then((res) => res.json())
+        .then(data => {
+            if(!data.success && data.success !== undefined) {
+                alert('Enter valid username')
+                event.target.value = ''
+                return;
+            }
+            const navbar = document.getElementById('navbar')
+            const loading = document.getElementById('loading-section')
+            navbar.className = 'hide-element'
+            loading.className = ''
+            let link = document.URL
+            console.log(' '+link)
+            window.location.href = `kyuList.html?user=${event.target.value}`
+        })
+    }
+  })
+})();
